a3b3861fcd55545eaa970d4e0d5d9ae4fc3ef5ff
Анна Петрачкова <p.anytk@mail.ru>
2021-08-03 10:49:37 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n);
+    let nStr = n.toString();
+    let res = 0;
+    let count = 1;
+    for (let i = 0; i < nStr.length - 1; i++){
+        count *= 10;
+    }
+    for (let i = 0; i < nStr.length; i++) {
+        let ost = n % 10;
+        res = res + ost * count;
+        count /= 10;
+        n = Math.trunc(n / 10);
+    }
+    return res;
 }
