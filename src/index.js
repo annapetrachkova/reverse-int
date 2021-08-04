5743cba689352b402b2386e98be75e5637f35440
Анна Петрачкова <p.anytk@mail.ru>
2021-08-04 22:02:03 +0300

    fix: some changes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
 module.exports = function reverse (n) {
-    n = Math.abs(n);
-    let nStr = n.toString();
-    let res = 0;
-    let count = 1;
-    for (let i = 0; i < nStr.length - 1; i++){
-        count *= 10;
-    }
-    for (let i = 0; i < nStr.length; i++) {
-        let ost = n % 10;
-        res = res + ost * count;
-        count /= 10;
-        n = Math.trunc(n / 10);
-    }
-    return res;
+     n = Math.abs(n);
+     let nString = n.toString();
+     let nArray = nString.split('');
+     let res = nArray.reverse();
+     return res.join('');
 }
